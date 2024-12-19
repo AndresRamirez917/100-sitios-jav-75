7ff7e03d4b20fed54714c43a94c69aca4de76677
AndresRamirez917 <andresmintic2021@gmail.com>
2024-12-19 14:56:32 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+async function getData() {
+    const result = await fetch('https://thecocktaildb.com/api/json/v1/1/search.php?s=margarita');
+    const coctail = await result.json();
+    const gallery = ["gallery1","gallery2","gallery3","gallery4","gallery5"];
+    const linksArr = ["1","2","3","4","5"];
+    const randCoctail = coctail.drinks.sort(() => 0.5 - Math.random()).slice(0, 5);
+    console.log(coctail);
+    randCoctail.forEach((element, index) => {
+        const box = document.createRange().createContextualFragment(`
+            
+            <div class="grid-box box-${linksArr[index]}">
+                <a href="${gallery[index]}.html"></a>
+                <img src="${element.strDrinkThumb}" alt="">
+            </div>
+            
+            `)
+            const gallery_grid = document.querySelector('.gallery-grid');
+            gallery_grid.append(box)
+    });
+}
+getData()
